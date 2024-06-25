b85282278d7f9ccbf9573ab4d300ea47493db0be
moamerkhalefa <moamer.khalefa5@gmail.com>
2024-06-25 23:14:06 +0300

    edit

--- src/app/authorized/guards/auth.guard.ts
+++ src/app/authorized/guards/auth.guard.ts
@@ -2,12 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  let router = inject(Router)
-  if(localStorage.getItem("token")) {
-    return true;
-  } else {
-    router.navigate(["/auth/login"]);
-
-    return false;
-  }
+  // let router = inject(Router)
+  // if(localStorage.getItem("token")) {
+  //   return true;
+  // } else {
+  //   router.navigate(["/auth/login"]);
+  //   return false;
+  // }
+  return true;
 };
